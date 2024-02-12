57f91614be804c7fe2d4f5b1827a23b1e10df7fe
pourushkg <pkg.21p10161@mtech.nitdgp.ac.in>
2024-02-12 11:52:33 +0530

    data ingestion code updated

---
+++ Xray/constant/training_pipeline/__init__.py
@@ -0,0 +1,21 @@
+
+import torch
+from datatime import datetime
+
+
+TIMESTAMP: datetime = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+
+# Data Ingestion Constants
+ARTIFACT_DIR: str = "artifacts"
+
+BUCKET_NAME: str = "lungxray24"
+
+S3_DATA_FOLDER: str = "data"
+
+
+# data trasnforamtion
+CLASS_LABEL_1: str = "NORMAL"
+
+CLASS_LABEL_2: str = "PNEUMONIA"
+
